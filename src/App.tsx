77bb2b8d15f5ea2c0be3bc0907c057e51163a299
Tiger Wang <hecool108@gmail.com>
2024-09-10 18:07:44 +0800

    refactor: move chrome tab logic to new file

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,12 @@ import { Box, Center, VStack } from '@chakra-ui/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { useMount } from 'ahooks';
 import { useState } from 'react';
-import { getARandomImg } from './on-page-content';
+import { onChromeTabActive } from './on-page-content';
 
 function App() {
   const [topImage, setTopImage] = useState<string>()
-  useMount(() => {
-    chrome.tabs.query(
-      { active: true, currentWindow: true },
-      (results: chrome.tabs.Tab[]) => {
-        if(results.length !== 0){
-          const [tab] = results
-          chrome.scripting.executeScript({
-            target:{tabId: tab.id!},
-            func:() => document.documentElement.outerHTML 
-          },(results:any[])=>{
-            const [data] = results;
-            if(data){
-              const imageSRC = getARandomImg(data.result);
-              setTopImage(imageSRC)
-            }
-          })
-        }
-      }
-    );
+  useMount(async() => {
+    setTopImage(await onChromeTabActive())
   });
   return (
     <ChakraProvider>
--- src/on-page-content.ts
+++ src/on-page-content.ts
@@ -1,7 +1,32 @@
 import * as cheerio from 'cheerio';
-export const getARandomImg = (htmlText:string)=>{
-    
-    const $ = cheerio.load(htmlText);
-    //do cheerio logic
-    return '';
-}
+export const getARandomImg = (htmlText: string) => {
+  const $ = cheerio.load(htmlText);
+  //do cheerio logic
+  return 'TBD';
+};
+
+export const onChromeTabActive = (): Promise<string> => {
+  return new Promise((resolve) => {
+    chrome.tabs.query(
+      { active: true, currentWindow: true },
+      (results: chrome.tabs.Tab[]) => {
+        if (results.length !== 0) {
+          const [tab] = results;
+          chrome.scripting.executeScript(
+            {
+              target: { tabId: tab.id! },
+              func: () => document.documentElement.outerHTML,
+            },
+            (results: any[]) => {
+              const [data] = results;
+              if (data) {
+                const imageSRC = getARandomImg(data.result);
+                resolve(imageSRC);
+              }
+            }
+          );
+        }
+      }
+    );
+  });
+};
